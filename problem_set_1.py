4ab0f5ebbb90947e1daadff3d2a83c909a2862c2
Tyson Bailey <onaclov2000+github@gmail.com>
2015-08-14 15:22:07 -0600

    Added Test Cases in

--- problem_set_1.py
+++ problem_set_1.py
@@ -47,12 +47,15 @@ def localize(colors,measurements,motions,sensor_right,p_move):
     # initializes p to a uniform distribution over a grid of the same dimensions as colors
     pinit = 1.0 / float(len(colors)) / float(len(colors[0]))
     p = [[pinit for row in range(len(colors[0]))] for col in range(len(colors))]
-    
-    # >>> Insert your code here <<<
+#     >>> Insert your code here <<<
     for i in range(len(motions)):
         p = move(p, motions[i])
+        #print "move"
+        #show(p)
+        #print "/move"
+        #print ""
+        #print ""
         p = sense(p,measurements[i], sensor_right)
-        
     return p
 
 def sense(p, Z, sensor_right):
@@ -77,19 +80,21 @@ def move(p, U):
     #  [0,1] - right
     if U == [0,1]:
        for i in range(len(p)):
-            p[i] = p[i][-1:] + p[i][:-1]
+          p[i] = p[i][1:] + p[i][:1]
+
     #  [1,0] - down
     if U == [1,0]:
-        p = p[1:] + p[:1]
+       p = p[-1:] + p[:-1]
     #  [-1,0] - up
     if U == [-1,0]:
-        p = p[-1:] + p[:-1]
+        p = p[1:] + p[:1]
     #  [0,-1] - left
     if U == [0,-1]:
-       for i in range(len(p)):
-          p[i] = p[i][1:] + p[i][:1]
-       print p
+        for i in range(len(p)):
+            p[i] = p[i][-1:] + p[i][:-1]       
+       
     return p
+
 
 def show(p):
     rows = ['[' + ','.join(map(lambda x: '{0:.5f}'.format(x),r)) + ']' for r in p]
@@ -103,16 +108,17 @@ def show(p):
 #  [0.00910, 0.00715, 0.01434, 0.04313, 0.03642]]
 # (within a tolerance of +/- 0.001 for each entry)
 
-#colors = [['R','G','G','R','R'],
-#          ['R','R','G','R','R'],
-#          ['R','R','G','G','R'],
-#          ['R','R','R','R','R']]
-#colors = [['R','G','G','R','R']]
-#measurements = ['G','G','G','G','G']
-#motions = [[0,0],[0,1],[1,0],[1,0],[0,1]]
-#motions = [[0,0],[0,0],[0,-1],[0,1],[0,-1]]
+colors = [['R','G','G','R','R'],
+          ['R','R','G','R','R'],
+          ['R','R','G','G','R'],
+          ['R','R','R','R','R']]
+measurements = ['G','G','G','G','G']
+motions = [[0,0],[0,1],[1,0],[1,0],[0,1]]
+p = localize(colors,measurements,motions,sensor_right = 0.7, p_move = 0.8)
+show(p) # displays your answer
 
-#p = localize(colors,measurements,motions,sensor_right = 0.7, p_move = 0.8)
+
+# test 1
 colors = [['G', 'G', 'G'],
           ['G', 'R', 'G'],
           ['G', 'G', 'G']]
@@ -121,7 +127,129 @@ motions = [[0,0]]
 sensor_right = 1.0
 p_move = 1.0
 p = localize(colors,measurements,motions,sensor_right,p_move)
-
-show(p) # displays your answer
-
+correct_answer = (
+    [[0.0, 0.0, 0.0],
+     [0.0, 1.0, 0.0],
+     [0.0, 0.0, 0.0]])
+if p == correct_answer:
+    print "passed test 1"
+else:
+    print "Failed Test 1"
+    show(p)
+    show(correct_answer)
     
+# test 2
+colors = [['G', 'G', 'G'],
+          ['G', 'R', 'R'],
+          ['G', 'G', 'G']]
+measurements = ['R']
+motions = [[0,0]]
+sensor_right = 1.0
+p_move = 1.0
+p = localize(colors,measurements,motions,sensor_right,p_move)
+correct_answer = (
+    [[0.0, 0.0, 0.0],
+     [0.0, 0.5, 0.5],
+     [0.0, 0.0, 0.0]])
+if p == correct_answer:
+    print "passed test 2"
+else:
+    print "Failed Test 2"
+    show(p)
+    show(correct_answer)
+    
+# test 3
+colors = [['G', 'G', 'G'],
+          ['G', 'R', 'R'],
+          ['G', 'G', 'G']]
+measurements = ['R']
+motions = [[0,0]]
+sensor_right = 0.8
+p_move = 1.0
+p = localize(colors,measurements,motions,sensor_right,p_move)
+correct_answer = (
+    [[0.06666666666, 0.06666666666, 0.06666666666],
+     [0.06666666666, 0.26666666666, 0.26666666666],
+     [0.06666666666, 0.06666666666, 0.06666666666]])
+if p == correct_answer:
+    print "passed test 3"
+else:
+    print "Failed Test 3"
+    show(p)
+    show(correct_answer)
+# test 4
+colors = [['G', 'G', 'G'],
+          ['G', 'R', 'R'],
+          ['G', 'G', 'G']]
+measurements = ['R', 'R']
+motions = [[0,0], [0,1]]
+sensor_right = 0.8
+p_move = 1.0
+p = localize(colors,measurements,motions,sensor_right,p_move)
+correct_answer = (
+    [[0.03333333333, 0.03333333333, 0.03333333333],
+     [0.13333333333, 0.13333333333, 0.53333333333],
+     [0.03333333333, 0.03333333333, 0.03333333333]])
+if p == correct_answer:
+    print "passed test 4"
+else:
+    print "Failed Test 4"
+    show(p)
+    show(correct_answer)
+# test 5
+colors = [['G', 'G', 'G'],
+          ['G', 'R', 'R'],
+          ['G', 'G', 'G']]
+measurements = ['R', 'R']
+motions = [[0,0], [0,1]]
+sensor_right = 1.0
+p_move = 1.0
+p = localize(colors,measurements,motions,sensor_right,p_move)
+correct_answer = (
+    [[0.0, 0.0, 0.0],
+     [0.0, 0.0, 1.0],
+     [0.0, 0.0, 0.0]])
+if p == correct_answer:
+    print "passed test 5"
+else:
+    print "Failed Test 5"
+    show(p)
+    show(correct_answer)
+# test 6
+colors = [['G', 'G', 'G'],
+          ['G', 'R', 'R'],
+          ['G', 'G', 'G']]
+measurements = ['R', 'R']
+motions = [[0,0], [0,1]]
+sensor_right = 0.8
+p_move = 0.5
+p = localize(colors,measurements,motions,sensor_right,p_move)
+correct_answer = (
+    [[0.0289855072, 0.0289855072, 0.0289855072],
+     [0.0724637681, 0.2898550724, 0.4637681159],
+     [0.0289855072, 0.0289855072, 0.0289855072]])
+if p == correct_answer:
+    print "passed test 6"
+else:
+    print "Failed Test 6"
+    show(p)
+    show(correct_answer)
+# test 7
+colors = [['G', 'G', 'G'],
+          ['G', 'R', 'R'],
+          ['G', 'G', 'G']]
+measurements = ['R', 'R']
+motions = [[0,0], [0,1]]
+sensor_right = 1.0
+p_move = 0.5
+p = localize(colors,measurements,motions,sensor_right,p_move)
+correct_answer = (
+    [[0.0, 0.0, 0.0],
+     [0.0, 0.33333333, 0.66666666],
+     [0.0, 0.0, 0.0]])
+if p == correct_answer:
+    print "passed test 6"
+else:
+    print "Failed Test 6"
+    show(p)
+    show(correct_answer)
