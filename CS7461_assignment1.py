d39a31ff742c155dd706888e409d9aa637d5e256
Tyson Bailey <onaclov2000+github@gmail.com>
2015-09-01 18:04:57 -0600

    Update CS7461_assignment1.py

--- CS7461_assignment1.py
+++ CS7461_assignment1.py
@@ -26,17 +26,18 @@ class Trainer():
         'coef':None,        #coef for Poly/Sigmoid defaults to 0
         'gamma':None,       #kernel param for poly/rbf/sigma - default is 1/#samples       
         }
-        svm = SVMClassifier(extractors, properties=mSVMProperties)
+        nn = NeuralNetworkClassifier(extractors)
+        #svm = SVMClassifier(extractors, properties=mSVMProperties)
         #svm = SVMClassifier(extractors, properties)
         # There are a bunch of options to try out.
-        tree = TreeClassifier(extractors)
-        bagged = TreeClassifier(extractors, flavor='Bagged')
-        boosted = TreeClassifier(extractors, flavor='Boosted')
-        forest = TreeClassifier(extractors, flavor='Forest')
-        bayes = NaiveBayesClassifier(extractors)
-        onenn = KNNClassifier(extractors)
-        twonn = KNNClassifier(extractors, k=2)
-        threenn = KNNClassifier(extractors, k=4)
+        #tree = TreeClassifier(extractors)
+        #bagged = TreeClassifier(extractors, flavor='Bagged')
+        #boosted = TreeClassifier(extractors, flavor='Boosted')
+        #forest = TreeClassifier(extractors, flavor='Forest')
+        #bayes = NaiveBayesClassifier(extractors)
+        #onenn = KNNClassifier(extractors)
+        #twonn = KNNClassifier(extractors, k=2)
+        #threenn = KNNClassifier(extractors, k=4)
         # Oddly the 3 KNN classifiers below have some kind of erro in the implementation I chose.
         #twonn_dist_Euclidean = KNNClassifier(extractors, k=2, dist='Euclidean')
         #twonn_dist_Manhattan = KNNClassifier(extractors, k=2, dist='Manhattan')
